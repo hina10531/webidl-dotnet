b02d7e48e5cf70cb1f1e133d0a26cf109a2fe6cb
juan.manuel.lallana@gmail.com <juan.manuel.lallana@gmail.com@6a60c55b-eb69-19be-78b0-c0ea3d9ef3f9>
2012-03-10 16:41:14 +0000

    Google translator into English: Initial rise of the project for testing.

---
+++ trunk/test/DefinitionTest.cs
@@ -0,0 +1,16 @@
+using System;
+using NUnit.Framework;
+
+namespace WebIDL.Test
+{
+	[TestFixture()]
+	public class DefinitionTest
+	{
+		[Test()]
+		public void CreateFormFile()
+		{
+			
+		}
+	}
+}
+
