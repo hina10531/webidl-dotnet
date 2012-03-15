5c22c9611e4965c2dad16ba1ddec26dee91be71c
juan.manuel.lallana <juan.manuel.lallana@6a60c55b-eb69-19be-78b0-c0ea3d9ef3f9>
2012-03-15 00:58:11 +0000

    Enumerations now throw an exception when they have repeated values​​. Added the corresponding test.

--- Enumerate.cs
+++ Enumerate.cs
@@ -7,12 +7,24 @@ namespace WebIDL
 {
 	public class Enumerate : Definition
 	{
+		public class RepeatValueException : System.Exception
+		{
+			public RepeatValueException(string enumname, string enumvalue):base("Value +" + enumvalue + " already exists in enum " + enumname)
+			{
+			}
+		}
+		
 		public ReadOnlyCollection<string> Values;
 		internal Enumerate (CommonTree tree,IContainer parent):base(tree,parent)
 		{
 			var aux = new List<string>();
 			foreach(var child in (tree.Children[1] as CommonTree).Children)
-				aux.Add(child.Text.Substring(1,child.Text.Length - 2));
+			{
+				var item = child.Text.Substring(1,child.Text.Length - 2);
+				if(aux.Contains(item))
+					throw new RepeatValueException(this.Name,item);
+				aux.Add(item);
+			}
 			Values = aux.AsReadOnly();
 		}
 	}
